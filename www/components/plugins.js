b0cc3bbd3e0f101d12b01ee81779f01d53d69d46
Bernardo112 <bernardo.cruz@etec.sp.gov.br>
2023-05-17 02:33:21 +0900

    Project's Initial Commit

---
+++ www/components/plugins.js
@@ -0,0 +1,21 @@
+// This is a JavaScript file
+
+function geolocationF(){
+
+    var onSuccess = function(position){
+            document.getElementById("latitude").value = position.coords.latitude;
+            document.getElementById("longitude").value = position.coords.longitude;
+            /*alert(
+                'Latitude: ' + position.coords.latitude + '\n' + 
+                'Longitude: ' + position.coords.longitude + '\n'
+            );*/
+    };
+
+    function onError(error){
+        alert('code: ' + error.code + '\n' + 
+              'message' + error.message
+        );
+    }
+
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
